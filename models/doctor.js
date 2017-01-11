6e7d269ece782faaf1dd069a88ee1e7a0e617608
Rod Reyes <roderick.reyes@gmail.com>
2017-01-11 19:34:32 +0100

    refactor: update doctor schema

--- models/doctor.js
+++ models/doctor.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('Doctor', {
-  user_id: Number,
+  user_id: Schema.Types.ObjectId,
   gender: { type: String, enum: ['M', 'F', 'O'] },
   birthdate: { type: Date, max: Date.now() },
-  treatment_certs: [Number]
+  treatment_certs: [Schema.Types.ObjectId]
 });

a4c909c1488a2fb0d6985628fff4bc925742409c
Rod Reyes <roderick.reyes@gmail.com>
2017-01-12 11:09:17 +0100

    refactor: remove patient specific field from user schema

--- models/user.js
+++ models/user.js
@@ -14,11 +14,4 @@ module.exports = mongoose.model('User', {
   gender: { type: String, enum: ['M', 'F', 'O'] },
   birthdate: { type: Date, max: Date.now() },
   user_type: { type: String, enum: ['doctor', 'nurse', 'patient'] },
-  user_type_fields: {
-    patient: {
-      conditions: { type: String, enum: ['condition1', 'condition2', 'condition3'] },
-      medications: { type: String, enum: ['medication1', 'medication2', 'medication3'] },
-      treatments: { type: String, enum: ['treatment1', 'treatment2', 'treatment3'] }
-    },
-  }
 });

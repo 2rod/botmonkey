38b42e4ca91f6728d16202643dd7371fc9be8ea6
Rod Reyes <roderick.reyes@gmail.com>
2017-01-13 21:02:23 +0100

    feat: add routes for user schema operations

--- routes.js
+++ routes.js
@@ -1,35 +1,43 @@
-"use strict";
+'use strict';
 
-const config = require("./config.json");
+const config = require('./config.json');
 
-const app = require("./index.js").app;
-const passport = require("./index.js").passport;
-const Router = require("koa-router");
+const app = require('./index.js').app;
+const passport = require('./index.js').passport;
+const Router = require('koa-router');
 
 const routes = new Router();
 
-const main = require("./controllers/main.js");
-const account = require("./controllers/account.js");
+const main = require('./controllers/main.js');
+const account = require('./controllers/account.js');
+const userCtrl = require('./controllers/userCtrl.js');
 
 // routes
 
-routes.get("/", main.index);
+routes.get('/', main.index);
 
 // for passport
-routes.get("/login", account.login);
-routes.get("/logout", account.logout);
-routes.get("/account", account.index);
+routes.get('/login', account.login);
+routes.get('/logout', account.logout);
+routes.get('/account', account.index);
 
 // you can add as many strategies as you want
-routes.get("/auth/github",
-	passport.authenticate("github")
+routes.get('/auth/github',
+  passport.authenticate('github')
 );
 
-routes.get("/auth/github/callback",
-	passport.authenticate("github", {
-		successRedirect: "/account",
-		failureRedirect: "/"
-	})
+routes.get('/auth/github/callback',
+  passport.authenticate('github', {
+    successRedirect: '/account',
+    failureRedirect: '/'
+  })
 );
+
+// user routes
+routes.get('/users', userCtrl.getAllUsers);
+routes.get('/users/:user_type', userCtrl.getUsersByType);
+routes.post('/user/search', userCtrl.getUsers);
+routes.post('/user/search/fullname', userCtrl.getUserByFullName);
+routes.get('/users/:user_type/:gender', userCtrl.getUsersByGender);
 
 app.use(routes.middleware());

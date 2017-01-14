136b855d5927ee378a89bc9e336825e937c34153
Rod Reyes <roderick.reyes@gmail.com>
2017-01-14 12:00:42 +0100

    refactor: remove required property from case_number field in appointment schema

--- models/appointment.js
+++ models/appointment.js
@@ -8,7 +8,7 @@ const appointmentSchema = mongoose.Schema({
   condition_treated: { type: String, enum: ['condition1', 'condition2', 'condition3'] },
   treatment_given: { type: String, enum: ['treatment1', 'treatment2', 'treatment3'] },
   medication_prescribed: { type: String, enum: ['medication1', 'medication2', 'medication3'] },
-  case_number: { type: Number, required: true }
+  case_number: { type: Number }
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);

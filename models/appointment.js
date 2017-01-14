07b332173511f05bc6660c5e03843e94e2b5473c
Rod Reyes <roderick.reyes@gmail.com>
2017-01-14 11:06:10 +0100

    refactor: add new case_number field to appointment schema

--- models/appointment.js
+++ models/appointment.js
@@ -7,7 +7,8 @@ const appointmentSchema = mongoose.Schema({
   type: { type: String, enum: ['initial', 'follow-up', 'maintenance', 'final'] },
   condition_treated: { type: String, enum: ['condition1', 'condition2', 'condition3'] },
   treatment_given: { type: String, enum: ['treatment1', 'treatment2', 'treatment3'] },
-  medication_prescribed: { type: String, enum: ['medication1', 'medication2', 'medication3'] }
+  medication_prescribed: { type: String, enum: ['medication1', 'medication2', 'medication3'] },
+  case_number: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);

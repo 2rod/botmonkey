536c7bb40ce3988dbcf36d09c38a71083300e1e5
Rod Reyes <roderick.reyes@gmail.com>
2017-01-16 13:23:07 +0100

    refactor: simplify architecture by integrating vitals and case number fields into appointment record

--- models/appointment.js
+++ models/appointment.js
@@ -8,7 +8,22 @@ const appointmentSchema = mongoose.Schema({
   type: { type: String, enum: ['initial', 'follow-up', 'maintenance', 'final'] },
   condition_treated: { type: String, enum: ['condition1', 'condition2', 'condition3'] },
   treatment_given: { type: String, enum: ['treatment1', 'treatment2', 'treatment3'] },
-  medication_prescribed: { type: String, enum: ['medication1', 'medication2', 'medication3'] }
+  medication_prescribed: { type: String, enum: ['medication1', 'medication2', 'medication3'] },
+  post_screening: {
+    vital_1: {
+      name: String,
+      value: Number
+    },
+    vital_2: {
+      name: String,
+      value: Number
+    },
+    vital_3: {
+      name: String,
+      value: Number
+    }
+  },
+  case_number: { type: Number }
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);

472c111525def52eab22d37f1909809980e254e5
Rod Reyes <roderick.reyes@gmail.com>
2017-01-16 11:08:51 +0100

    refactor: update appointment schema: require mongoose Schema; require Date field; remove case number field

--- models/appointment.js
+++ models/appointment.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const appointmentSchema = mongoose.Schema({
-  date: Date,
+  date: { type: Date, required: true },
   patient_id: { type: Schema.Types.ObjectId, ref: 'User' },
   doctor_id: { type: Schema.Types.ObjectId, ref: 'User' },
   type: { type: String, enum: ['initial', 'follow-up', 'maintenance', 'final'] },
   condition_treated: { type: String, enum: ['condition1', 'condition2', 'condition3'] },
   treatment_given: { type: String, enum: ['treatment1', 'treatment2', 'treatment3'] },
-  medication_prescribed: { type: String, enum: ['medication1', 'medication2', 'medication3'] },
-  case_number: { type: Number }
+  medication_prescribed: { type: String, enum: ['medication1', 'medication2', 'medication3'] }
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);

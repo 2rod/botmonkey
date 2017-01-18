03c774f47bb19be2af83e11334773fd772e44fe4
Rod Reyes <roderick.reyes@gmail.com>
2017-01-18 11:21:14 +0100

    refactor: add proper references to config settings using nconf in account.js; code cleanup

--- controllers/account.js
+++ controllers/account.js
@@ -1,28 +1,29 @@
-"use strict";
+'use strict';
 
-const config = require("../config.json");
+const config = require('../nconf');
 
 let user = null;
 
-module.exports.login = function* login() {
-	if (this.isAuthenticated()) {
-		user = this.session.passport.user;
-	}
-	yield this.render("login", {
-		user: user
-	});
+module.exports.login = function* login () {
+  if (this.isAuthenticated()) {
+    user = this.session.passport.user;
+  }
+  yield this.render('login', {user: user});
 };
 
-module.exports.logout = function* logout() {
-	this.logout();
-	yield this.redirect("/");
+module.exports.logout = function* logout () {
+  this.logout();
+  yield this.redirect('/');
 };
 
-module.exports.index = function* index() {
-	if (this.isAuthenticated()) {
-		user = this.session.passport.user;
-	} else {
-		return this.redirect("/");
-	}
-	yield this.render("account", {title: config.site.name, user: JSON.stringify(user, null, 2)});
+module.exports.index = function* index () {
+  if (this.isAuthenticated()) {
+    user = this.session.passport.user;
+  } else {
+    return this.redirect('/');
+  }
+  yield this.render('account', {
+    title: config.SITE_NAME,
+    user: JSON.stringify(user, null, 2)
+  });
 };

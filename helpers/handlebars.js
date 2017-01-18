ff9875c98b3d7d93b5f8834bc646bf6a5e6b8563
Rod Reyes <roderick.reyes@gmail.com>
2017-01-18 11:31:13 +0100

    refactor: add proper references to config settings using nconf in handlebars.js; code cleanup

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,61 +1,65 @@
-"use strict";
+'use strict';
 
-const hbs = require("koa-hbs");
-const config = require("../config.json");
+const hbs = require('koa-hbs');
+const config = require('../nconf');
 
-hbs.registerHelper("if_eq", function if_eq(a, b, opts) {
-	if (a == b) {
-		return opts.fn(this);
-	}
-	return opts.inverse(this);
+hbs.registerHelper('if_eq', function if_eq(a, b, opts) {
+  if (a == b) {
+    return opts.fn(this);
+  }
+  return opts.inverse(this);
 });
 
-hbs.registerHelper("copyright_year", (opts) => {
-	return new Date().getFullYear();
+hbs.registerHelper('copyright_year', (opts) => {
+  return new Date().getFullYear();
 });
 
-hbs.registerHelper("get_name", (opts) => {
-	return config.site.name;
+hbs.registerHelper('get_name', (opts) => {
+  return config.SITE_NAME;
 });
 
-hbs.registerHelper("get_analytics", (opts) => {
-	if (config.site.analytics) {
-		return config.site.analytics;
-	}
+hbs.registerHelper('get_analytics', (opts) => {
+  if (config.SITE_ANALYTICS) {
+    return config.SITE_ANALYTICS;
+  }
 });
 
-hbs.registerHelper("has_analytics", function has_analytics(opts) {
-	const fnTrue = opts.fn;
-	const fnFalse = opts.inverse;
-	return (config.site.analytics && config.site.analytics !== false) ? fnTrue() : fnFalse();
+hbs.registerHelper('has_analytics', function has_analytics(opts) {
+  const fnTrue = opts.fn;
+  const fnFalse = opts.inverse;
+  return (config.SITE_ANALYTICS && config.SITE_ANALYTICS !== false)
+      ? fnTrue()
+      : fnFalse();
 });
 
-hbs.registerHelper("has_fb_sdk", function has_fb_sdk(opts) {
-	const fnTrue = opts.fn;
-	const fnFalse = opts.inverse;
-	return (config.site.fb_sdk && config.site.fb_sdk.enable !== false) ? fnTrue() : fnFalse();
+hbs.registerHelper('has_fb_sdk', function has_fb_sdk (opts) {
+  const fnTrue = opts.fn;
+  const fnFalse = opts.inverse;
+  return (FB_SDK_ENABLE !== false)
+      ? fnTrue()
+      : fnFalse();
 });
 
-hbs.registerHelper("get_fb_page_name", (opts) => {
-	if (config.site.fb_sdk.enable) {
-		return config.site.fb_sdk.fb_page_name;
-	}
+hbs.registerHelper('get_fb_page_name', (opts) => {
+  if (config.FB_SDK_ENABLE) {
+    return config.FB_PAGE_NAME;
+  }
 });
 
-hbs.registerHelper("get_fb_page_title", (opts) => {
-	if (config.site.fb_sdk.enable) {
-		return config.site.fb_sdk.fb_page_title;
-	}
+hbs.registerHelper('get_fb_page_title', (opts) => {
+  if (config.FB_SDK_ENABLE) {
+    return config.FB_PAGE_TITLE;
+  }
 });
 
-hbs.registerHelper("get_fb_app_id", (opts) => {
-	if (config.site.fb_sdk.enable) {
-		return config.site.fb_sdk.fb_app_id;
-	}
+hbs.registerHelper('get_fb_app_id', (opts) => {
+  if (config.FB_SDK_ENABLE) {
+    return config.FB_APP_ID;
+  }
 });
 
-hbs.registerHelper("get_fb_page_id", (opts) => {
-	if (config.site.fb_sdk.enable) {
-		return config.site.fb_sdk.fb_page_id;
-	}
+hbs.registerHelper('get_fb_page_id', (opts) => {
+  if (config.FB_SDK_ENABLE) {
+    return config.FB_PAGE_ID;
+  }
 });

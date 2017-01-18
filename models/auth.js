9d28f2e3be6a5e54547f3a3ffabcb2b2237e8061
Rod Reyes <roderick.reyes@gmail.com>
2017-01-18 11:37:11 +0100

    refactor: add proper references to config settings using nconf in auth.js; code cleanup

--- models/auth.js
+++ models/auth.js
@@ -1,35 +1,35 @@
-"use strict";
+'use strict';
 
-const passport = require("../index.js").passport;
-const config = require("../config.json");
-const co = require("co");
+const passport = require('../index.js').passport;
+const config = require('../nconf');
+const co = require('co');
 
 passport.serializeUser((user, done) => {
-	done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-	done(null, user);
+  done(null, user);
 });
 
-const GithubStrategy = require("passport-github").Strategy;
+const GithubStrategy = require('passport-github').Strategy;
 // if we have a port other than 80, add it to our callback url
-let port = "";
-if (config.site.port !== 80) {
-	port = `:${config.site.port}`;
+let port = '';
+if (config.PORT !== 80) {
+  port = `:${config.PORT}`;
 }
 passport.use(new GithubStrategy({
-	clientID: config.site.oauth.github.clientID,
-	clientSecret: config.site.oauth.github.clientSecret,
-	callbackURL: `${config.site.oauth.host}${port}/auth/github/callback`
+  clientID: config.GITHUB.clientID,
+  clientSecret: config.GITHUB.clientSecret,
+  callbackURL: `${config.OAUTH_HOST}${port}/auth/github/callback`
 }, (token, tokenSecret, profile, done) => {
-	// retrieve user ...
-	co(function* auth() {
-		// do some async/yield stuff here to get/set profile data
-		done(null, profile);
-	}).catch(function onError(e) {
-		console.error("Something went terribly wrong!");
-		console.error(e.stack);
-		done(e, null);
-	});
+  // retrieve user ...
+  co(function* auth () {
+    // do some async/yield stuff here to get/set profile data
+    done(null, profile);
+  }).catch(function onError (e) {
+    console.error('Something went terribly wrong!');
+    console.error(e.stack);
+    done(e, null);
+  });
 }));

69b04b839865d5e029ab251dd5bad605e5b8dbb1
Rod Reyes <roderick.reyes@gmail.com>
2017-01-18 10:39:35 +0100

    chore: add new mongodb env var for heroku app

--- helpers/db.js
+++ helpers/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const url = ('mongodb://localhost:27017/botmonkey');
+const url = (process.env.MONGODB_URI) ? process.env.MONGODB_URI : 'mongodb://localhost:27017/botmonkey';
 
 mongoose.connect(url, function (err, db) {
   if (err) console.error('error connecting: ' + err.stack);
